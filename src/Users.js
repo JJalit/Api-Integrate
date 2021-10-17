db1f4cfaf2e582f371ac72dd84dab95bdf675221
jaeha <jaeha23@naver.com>
2021-10-17 16:35:53 +0900

    finish useAsync

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+
 import useAsync from "./useAsync";
+import User from "./User";
 
 async function getUsers() {
   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
@@ -8,6 +10,7 @@ async function getUsers() {
 }
 
 function Users() {
+  const [userId, setUserId] = useState(null);
   const [state, refetch] = useAsync(getUsers, [], true);
 
   const { loading, data: users, error } = state;
@@ -20,12 +23,13 @@ function Users() {
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)} style={{ cursor: "pointer" }}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
